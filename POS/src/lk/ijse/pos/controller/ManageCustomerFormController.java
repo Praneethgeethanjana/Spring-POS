856ca491abd70cf3e933a6a2acbe16b133665467
Praneethgeethanjana <geethanjana10@gmail.com>
2021-08-26 23:31:32 +0530

    customer

--- POS/src/lk/ijse/pos/controller/ManageCustomerFormController.java
+++ POS/src/lk/ijse/pos/controller/ManageCustomerFormController.java
@@ -16,15 +16,11 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import lk.ijse.pos.AppInitializer;
-import lk.ijse.pos.db.DBConnection;
+import lk.ijse.pos.dao.CustomerDAOImpl;
+import lk.ijse.pos.model.Customer;
 import lk.ijse.pos.view.tblmodel.CustomerTM;
 
-
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -51,38 +47,25 @@ public class ManageCustomerFormController implements Initializable {
     private TableView<CustomerTM> tblCustomers;
 
     private void loadAllCustomers() {
-
         try {
 
-            Connection connection = DBConnection.getInstance().getConnection();
-            Statement stm = connection.createStatement();
-            ResultSet rst = stm.executeQuery("SELECT * FROM Customer");
-            ArrayList<CustomerTM> alCustomers = new ArrayList<>();
+            CustomerDAOImpl customerDAO = new CustomerDAOImpl();
+            ArrayList<Customer> allCustomers = customerDAO.getAllCustomers();
+            ArrayList<CustomerTM> allCustomersForTable = new ArrayList<>();
 
-            while (rst.next()) {
-
-                CustomerTM customer = new CustomerTM(
-                        rst.getString(1),
-                        rst.getString(2),
-                        rst.getString(3));
-
-                alCustomers.add(customer);
-
+            for (Customer customer : allCustomers) {
+                allCustomersForTable.add(new CustomerTM(customer.getcID(), customer.getName(), customer.getAddress()));
             }
-
-            ObservableList<CustomerTM> olCustomers = FXCollections.observableArrayList(alCustomers);
-
+            ObservableList<CustomerTM> olCustomers = FXCollections.observableArrayList(allCustomersForTable);
             tblCustomers.setItems(olCustomers);
 
-        } catch (Exception ex) {
-            Logger.getLogger(ManageCustomerFormController.class.getName()).log(Level.SEVERE, null, ex);
-        }
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    /**
-     * Initializes the controller class.
-     */
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         tblCustomers.getColumns().get(0).setStyle("-fx-alignment:center");
@@ -129,14 +112,11 @@ public class ManageCustomerFormController implements Initializable {
             String customerID = tblCustomers.getSelectionModel().getSelectedItem().getId();
 
             try {
-                Connection connection = DBConnection.getInstance().getConnection();
 
-                PreparedStatement pstm = connection.prepareStatement("DELETE FROM Customer WHERE id=?");
-                pstm.setObject(1, customerID);
+                CustomerDAOImpl customerDAO = new CustomerDAOImpl();
+                boolean b = customerDAO.deleteCustomer(customerID);
 
-                int affectedRows = pstm.executeUpdate();
-
-                if (affectedRows > 0) {
+                if (b) {
                     loadAllCustomers();
                 } else {
                     Alert a = new Alert(Alert.AlertType.ERROR, "Failed to delete the customer", ButtonType.OK);
@@ -168,20 +148,11 @@ public class ManageCustomerFormController implements Initializable {
     private void btnSave_OnAction(ActionEvent event) {
 
         if (addnew) {
-
             try {
-                Connection connection = DBConnection.getInstance().getConnection();
 
-                PreparedStatement pstm = connection.prepareStatement("INSERT INTO Customer VALUES (?,?,?,?)");
-
-                pstm.setObject(1, txtCustomerId.getText());
-                pstm.setObject(2, txtCustomerName.getText());
-                pstm.setObject(3, txtCustomerAddress.getText());
-                pstm.setObject(4, 0);
-
-                int affectedRows = pstm.executeUpdate();
-
-                if (affectedRows > 0) {
+                CustomerDAOImpl dao = new CustomerDAOImpl();
+                boolean b = dao.addCustomer(new Customer(txtCustomerId.getText(), txtCustomerName.getText(), txtCustomerAddress.getText()));
+                if (b) {
                     loadAllCustomers();
                 } else {
                     new Alert(Alert.AlertType.ERROR, "Unable to add new customer", ButtonType.OK).show();
@@ -192,17 +163,10 @@ public class ManageCustomerFormController implements Initializable {
 
         } else {
             try {
-                //Update
-                Connection connection = DBConnection.getInstance().getConnection();
 
-                PreparedStatement pstm = connection.prepareStatement("UPDATE Customer SET name=?, address=? WHERE id=?");
-                pstm.setObject(1, txtCustomerName.getText());
-                pstm.setObject(2, txtCustomerAddress.getText());
-                pstm.setObject(3, txtCustomerId.getText());
-
-                int affectedRows = pstm.executeUpdate();
-
-                if (affectedRows > 0) {
+                CustomerDAOImpl dao = new CustomerDAOImpl();
+                boolean b = dao.updateCustomer(new Customer(txtCustomerId.getText(), txtCustomerName.getText(), txtCustomerAddress.getText()));
+                if (b) {
                     loadAllCustomers();
                 } else {
                     new Alert(Alert.AlertType.ERROR, "Unable to update the customer", ButtonType.OK).show();
